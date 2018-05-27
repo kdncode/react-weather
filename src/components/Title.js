cb496ace7a1d17b7cb98f440f982b5741db5e021
kdncode <kdncode@gmail.com>
2018-05-27 02:10:40 -0500

    set up components

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather Finder</h1>
+                <p>Find out your favorite city's weather</p>
+            </div>
+        );
+    }
+}
+
+export default Title;
