d9abb109edeba8d9ff50bfd567e6de9844b97b8c
kdncode <kdncode@gmail.com>
2018-05-27 03:52:16 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ const API_KEY = '7be5cee53b873db905098ce620e5de18'
 
 class App extends Component {
 
+	state = {
+		temperature: undefined,
+		city: undefined,
+		country: undefined,
+		humidity: undefined,
+		description: undefined,
+		error: undefined
+	}
+
 	getWeather = async (e) => {
 		e.preventDefault();
 		const city = e.target.elements.city.value;
@@ -16,6 +25,15 @@ class App extends Component {
 		const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY }&units=metric`);
 		const data = await api_call.json();
 		console.log(data);
+
+		this.setState({
+			temperature: data.main.temp,
+			city: data.name,
+			country: data.sys.country,
+			humidity: data.main.humidity,
+			description: data.weather[0].description,
+			error: ''
+		})
 	}
 
 	render() {
@@ -28,7 +46,14 @@ class App extends Component {
 			
 			<Title />
 			<Form getWeather={this.getWeather} />
-			<Weather />
+			<Weather 
+				temperature={this.state.temperature}
+				city={this.state.city}
+				country={this.state.country}
+				humidity={this.state.humidity}
+				description={this.state.description}
+				error={this.state.error}
+			/>
 			</div>
 		);
 	}
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,12 @@ class Weather extends Component {
     render() {
         return (
             <div>
-                Weather
+                {this.props.city && <p>City: {this.props.city}</p>}
+                {this.props.country && <p>Country: {this.props.country}</p>}
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                {this.props.description && <p>Conditions: {this.props.description}</p>}
+                {/* <p>City: {this.props.error}</p> */}
             </div>
         );
     }
