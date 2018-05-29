4a802c6b8948895329d5c166bf50c40e2ca29daf
kdncode <kdncode@gmail.com>
2018-05-28 23:34:27 -0500

    updated

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Weather = (props) => (
     <div className="weather__info">
-        {props.city && <p className="weather__key">City: <span className="weather__value">{props.city}</span></p>}
+        {props.city && <p className="weather__key">City: <span className="weather__value"> {props.city} </span></p>}
         {props.country && <p className="weather__key">Country: <span className="weather__value">{props.country}</span></p>}
         {props.temperature && <p className="weather__key">Temperature: <span className="weather__value">{props.temperature} &#8457;</span></p>}
         {props.humidity && <p className="weather__key">Humidity: <span className="weather__value">{props.humidity}</span></p>}
